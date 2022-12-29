bf77e2fc1ab257629f5fb4104c85956893e7d673
Antwan Olatunji <antwanola29@gmail.com>
2022-12-29 02:01:59 +0100

    Update Global.styled.js

--- src/components/styled/Global.styled.js
+++ src/components/styled/Global.styled.js
@@ -1,6 +1,17 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
+/* Chrome, Safari, Edge, Opera */
+input::-webkit-outer-spin-button,
+input::-webkit-inner-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
+}
+
+/* Firefox */
+input[type=number] {
+  -moz-appearance: textfield;
+}
     *, *::before, *::after{
         box-sizing: border-box;
     }
